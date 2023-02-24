cc100796fc32dadd7f53293329d5eaf3c4a93f36
汪军梁 <1428637884@qq.com>
2023-02-24 11:19:16 +0800

    feat:DP

--- findLongestPalindrome.js
+++ findLongestPalindrome.js
@@ -3,7 +3,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-02-22 11:00:23
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-02-23 15:04:03
+ * @LastEditTime: 2023-02-24 11:16:01
  */
 
 /**
@@ -97,4 +97,43 @@ function centralDiffusion(s){
     }
   }
   return s.slice(start,end+1)
+}
+
+
+/**
+ * @description: 动态规划  TC:O(n^2) SC:O(n^2)
+ * @author: JunLiangWang
+ * @param {*} s  输入字符串
+ * @return {*}
+ */
+function dp(s){
+   //回文子串的特点即为反转然后仍一样(bab,翻转：bab)，因此该问题可转换为s与翻转后的s寻找
+   //最长子串问题，但需要排除一个特例(abc435cba,翻转：abc534cba)，虽说abc，cba满足了翻转
+   //后一致，但并不是回文子串。
+
+   //DP数组，加一为了处理i-1,j-1的情况
+   let DPArray=new Array(s.length+1).fill(0).map(()=>new Array(s.length+1).fill(0))
+   let start=0,end=0
+   //由于DP数组长与宽都加了1，因此此处i,j都从1开始遍历
+   for(let i=1;i<=s.length;i++)
+   {
+     for(let j=1;j<=s.length;j++)
+     {
+       //比较s与翻转后的s的字符是否一致，一致则满足了回文串的特点
+       if(s[i-1]===s[s.length-j])
+       {
+          //当前子串长度=上一个子串长度+1
+          DPArray[i][j]=DPArray[i-1][j-1]+1
+          // 此处需要排除特列，即翻转后相同，但不是回文子串的情况。
+          // s.length-j即为未翻转时的起始位置，DPArray[i][j]为当前
+          // 子串长度，i为结束位置，当为回文子串时应满足(s.length-j)+DPArray[i][j]==i
+          if((s.length-j)+DPArray[i][j]==i&&DPArray[i][j]>end-start)
+          {
+             start=i-DPArray[i][j]
+             end=i
+          }
+       }
+     }
+   }
+   return s.substring(start,end)
 }
