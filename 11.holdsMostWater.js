6e8903760e2557cdca86ceb4cff1a6d4ba0fc658
汪军梁 <1428637884@qq.com>
2023-03-07 10:21:10 +0800

    perfect:add descripition

--- 11.holdsMostWater.js
+++ 11.holdsMostWater.js
@@ -1,9 +1,10 @@
 /*
- * @Description: 
+ * @Description: 给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+                 找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。返回容器可以储存的最大水量。
  * @Author: JunLiangWang
  * @Date: 2023-03-07 10:10:39
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-03-07 10:18:20
+ * @LastEditTime: 2023-03-07 10:20:16
  */
 
 /**
