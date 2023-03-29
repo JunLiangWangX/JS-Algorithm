e3103d2d6a1402d7b663a3636dd3bbcd92e04692
wangjl <1428637884@qq.com>
2023-03-29 14:08:13 +0800

    feat:add iteration methods

--- 24.pairwiseExchangeListNode.js
+++ 24.pairwiseExchangeListNode.js
@@ -4,7 +4,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-03-29 10:54:43
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-03-29 11:18:34
+ * @LastEditTime: 2023-03-29 14:07:15
  */
 
 
@@ -42,4 +42,43 @@ function recursionBacktracking(node)
     {
         return node;
     }
+}
+
+/**
+ * @description: 迭代法    TC:O(n)   SC:O(n)
+ * @author: JunLiangWang
+ * @param {*} head
+ * @return {*}
+ */
+function iteration(head){
+    /**
+     * 该方案利用迭代，每次遍历链表的两个节点，将其交换，直至遍历结束
+     */
+    // 如果当前节点为空或其后一个节点为空，证明无需要交换的节点，直接返回当前节点即可
+    if(!head||!head.next)return head;
+    // 新的头节点为原链表的第二个节点
+    const NEW_HEAD=head.next;
+    // 上一个节点初值为空
+    let lastNode=null;
+    // 当前节点初值为原链表第一个节点
+    let currentNode=head;
+    // 当前节点不为空且其下一个节点也不为空，证明仍存在需要交换的节点
+    while(currentNode&&currentNode.next)
+    {
+        // 将当前节点的下一个节点取出保存(nextNode)
+        let nextNode=currentNode.next;
+        // 将当前节点的下一个节点更新为nextNode的下一个节点
+        currentNode.next=nextNode.next;
+        // nextNode的下一个节点更新为当前节点，此时位置已完成交换
+        nextNode.next=currentNode;
+        // 如果存在上一个节点，则需要将上一个节点的下一个节点更新为
+        // 已经交换位置的nextNode
+        if(lastNode)lastNode.next=nextNode;
+        // 上一个节点更新为当前节点
+        lastNode=currentNode;
+        // 当前节点更新为其下一个节点
+        currentNode=currentNode.next;
+    }
+    // 返回新链表
+    return NEW_HEAD;
 }
