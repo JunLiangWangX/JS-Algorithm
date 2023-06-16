ef5e3a3a8f915873e2055eecd906011fddd0c27c
汪军梁 <1428637884@qq.com>
2023-06-16 09:54:29 +0800

    feat:add dp method

--- 70.ClimbingStairs.js
+++ 70.ClimbingStairs.js
@@ -4,7 +4,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-06-16 08:55:17
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-06-16 09:03:48
+ * @LastEditTime: 2023-06-16 09:51:57
  */
 
 
@@ -47,4 +47,44 @@ function dfs(n){
     recursionBackTrakcing(n);
     // 返回结果
     return methodCount;
+}
+
+
+/**
+ * @description: 动态规划   TC:O(n)  SC:O(1)
+ * @author: JunLiangWang
+ * @param {*} n 需要 n 阶你才能到达楼顶
+ * @return {*}
+ */
+function dp(n){
+    /**
+     * 该方案是用动态规划的方式，不难发现其实到达当前步数的方法是等于
+     * 到达前一步的方法加上到达前两步的方法，即：
+     * 
+     *          f[x]=f[x-1]+f[x-2]
+     * 
+     * 然后每移动一步：当前f[x-1]即等于上一次f[x]
+     *                当前f[x-2]即等于上一次f[x-1]
+     *                本次f[x]即通过公式即可计算得出
+     * 
+     * 因此我们定义三个遍历，模拟这个过程即可
+     */
+
+    // 到达前两步有多少种方法
+    let reachFirstTwoStepsWays=0,
+    // 到达前一步有多少种方法
+    reachPreviousStepWays=0,
+    // 到达现在有多少种方法
+    reachPresentWays=1;
+    // 遍历步数
+    for(let i=1;i<n;i++){
+        // 当前f[x-2]即等于上一次f[x-1]
+        reachFirstTwoStepsWays=reachPreviousStepWays;
+        // 当前f[x-1]即等于上一次f[x]
+        reachPreviousStepWays=reachPresentWays;
+        //  当前f[x]=f[x-1]+f[x-2]
+        reachPresentWays=reachFirstTwoStepsWays+reachPreviousStepWays;
+    }
+    // 返回结果
+    return reachPresentWays;
 }
