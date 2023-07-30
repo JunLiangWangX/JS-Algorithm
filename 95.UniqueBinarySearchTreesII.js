c9271578e402f92063ede2bd5175fa91769aa551
wangjl <1428637884@qq.com>
2023-07-30 15:31:49 +0800

    feat:add memorySearch method

--- 95.UniqueBinarySearchTreesII.js
+++ 95.UniqueBinarySearchTreesII.js
@@ -4,7 +4,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-07-30 15:04:05
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-07-30 15:20:31
+ * @LastEditTime: 2023-07-30 15:31:37
  */
 
 
@@ -25,7 +25,7 @@ function recursionBacktracking(n){
      * 根据以上特性，我们即可使用递归回溯的方式生成所有可能的二叉搜索树
      */
 
-    
+
     /**
      * @description: 递归
      * @author: JunLiangWang
@@ -53,7 +53,66 @@ function recursionBacktracking(n){
                 }
             }
         }
-        // 返回当前字节的二叉搜索树所有组合
+        // 返回当前范围的二叉搜索树所有组合
+        return nodeList;
+    }
+
+    // 执行递归
+    return recursion(1,n)
+}
+
+
+/**
+ * @description: 记忆搜索  TC:O(n^2)  SC:O(n^2)
+ * @author: JunLiangWang
+ * @param {*} n  给定整数n
+ * @return {*}
+ */
+function memorySearch(n){
+    /**
+     * 我们可对上述递归回溯进一步进行优化，上述递归回溯算法会
+     * 多次生成同一个[start,end]范围组合的二叉搜索树，我们可
+     * 以利用Map记录下已经生成的[start,end]范围的二叉搜索树
+     * 下次相同就无需再次生成了
+     */
+
+    // 利用Map记录下已经生成的[start,end]范围的二叉搜索树
+    let recordTree=new Map();
+    /**
+     * @description: 递归
+     * @author: JunLiangWang
+     * @param {*} start 节点值的开始范围
+     * @param {*} end   节点值的结束范围
+     * @return {*}
+     */    
+    function recursion(start,end){
+        // 当开始范围大于结束范围证明无已法生成节点，此时直接返回null
+        if(start>end)return [null]
+        // 查找该范围是否已经生成过
+        let nodeList=recordTree.get('s'+start+'e'+end);
+        // 已经生成过，直接返回上次生成的结果
+        if(nodeList!=undefined)return nodeList
+        // 没有则重置为空
+        else nodeList=[]
+
+        // 从[star,end]遍历选择头节点的值
+        for(let i=start;i<=end;i++){
+            // 以i为头节点，那么左树选择范围为[start,i-1]
+            let leftNodes=recursion(start,i-1),
+            // 以i为头节点，那么右树选择范围为[i+1,end]
+            rightNodes=recursion(i+1,end);
+            // 遍历所有左右节点，构造以i为头节点的二叉搜索树，
+            // 并记录到nodeList
+            for(let left of leftNodes)
+            {
+                for(let right of rightNodes){
+                    nodeList.push(new TreeNode(i,left,right))
+                }
+            }
+        }
+        // 记录当前范围的二叉搜索树所有组合
+        recordTree.set('s'+start+'e'+end,nodeList);
+        // 返回当前范围的二叉搜索树所有组合
         return nodeList;
     }
 
