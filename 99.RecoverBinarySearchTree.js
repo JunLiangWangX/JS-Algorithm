47fcd1d049a525823010d80bf4bb679ec87eefcf
汪军梁 <1428637884@qq.com>
2023-08-05 00:05:17 +0800

    feat:add InorderTraversalOptimization method

--- 99.RecoverBinarySearchTree.js
+++ 99.RecoverBinarySearchTree.js
@@ -4,7 +4,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-08-04 23:39:47
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-08-04 23:53:53
+ * @LastEditTime: 2023-08-05 00:04:08
  */
 
 
@@ -66,4 +66,64 @@ function InorderTraversal(root){
             return root;
         }
     }
+}
+
+
+/**
+ * @description: 中序遍历优化  TC:O(n)  SC:O(1)
+ * @author: JunLiangWang
+ * @param {*} root 给定树的根节点
+ * @return {*}
+ */
+function InorderTraversalOptimization(root){
+    /**
+     * 
+     * 其实我们仔细观察可以发现，其实待交换的两个
+     * 节点一个是第一个不满足单调递增的节点的前一
+     * 个节点，另一个节点则为最后一个不满足单调递
+     * 增的节点，因此我们可对上述中序遍历方案进行
+     * 空间上的优化
+     * 
+     */
+    // 记录上一个节点
+    let lastNode=null,
+    // 记录最后一个不满足单调递增的节点
+     errNode=null,
+     // 第一个不满足单调递增的节点的前一个节点
+    errLastNode=null;
+
+    /**
+     * @description: 中序遍历
+     * @author: JunLiangWang
+     * @param {*} root 当前节点
+     * @return {*}
+     */    
+    function recursion(root){
+        // 如果已到最后的子节点，则直接返回
+        if(!root)return ;
+        // 中序遍历——>递归左节点
+        recursion(root.left) 
+        // 当遇到不满足单调递增的节点
+        if(lastNode&&lastNode.val>root.val)
+        {
+            // 如果未找到第一个不满足单调递增的节点的前
+            // 一个节点则赋值，如果已找到则无需赋值
+            if(errLastNode==null)errLastNode=lastNode
+            // 更新最后一个不满足单调递增的节点为当前
+            // 节点
+            errNode=root
+        }
+        // 更新上一个节点为当前节点
+         lastNode=root
+         // 中序遍历——>递归右节点
+         recursion(root.right)
+    }
+    // 执行递归
+    recursion(root)
+    // 交换两节点值
+    let val=errLastNode.val
+    errLastNode.val=errNode.val
+    errNode.val=val
+    // 返回结果
+    return root;
 }
