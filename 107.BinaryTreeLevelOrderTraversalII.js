ce41c607241e13679fa67150fc2f30232380f90a
汪军梁 <1428637884@qq.com>
2023-08-17 09:07:16 +0800

    feat:add bfs method

--- 107.BinaryTreeLevelOrderTraversalII.js
+++ 107.BinaryTreeLevelOrderTraversalII.js
@@ -55,3 +55,57 @@ function dfs(root) {
 }
 
 
+
+/**
+ * @description: 广度优先  TC:O(n)  SC:O(n)
+ * @author: JunLiangWang
+ * @param {*} root  给定树的根节点
+ * @return {*}
+ */
+function bfs(root){
+    /**
+     * 本题与102.二叉树的层级遍历一样，只不过102题是从上往下层级
+     * 遍历，该题是从下往上，因此我们使用102题同样的方式，然后把
+     * 输出数组翻转即可
+     * 
+     * 该方案使用广度优先算法，该算法特征与题中描述一致，
+     * 从上到下从左到右逐层遍历。
+     * 
+     * 广度优先算法依靠一个队列，先将根节点入队，然后循环
+     * 不断将队列中的节点出队，然后将出队节点不为空的左右
+     * 节点再次入队。如此往复直到队列为空
+     * 
+     */
+
+    // 如果根为空，直接返回
+    if (!root) {
+        return [];
+    }
+    // 队列，并将根节点入队
+    const queue=[root],
+    // 输出数组
+    out=[]
+    // 当队列不为空则继续遍历
+    while(queue.length > 0){
+        // 记录当前队列长度，也是本次层级的
+        // 所有节点数量，由于出队会动态影响
+        // 到队列的长度，因此此处记录，就可
+        // 以知道本次层级的节点是否全部出队
+        let size=queue.length;
+        // 添加本次层级的数组
+        out.push([])
+        // 遍历当前层级的所有节点
+        while(size-->0)
+        {
+          // 出队
+          const  node=queue.shift();
+          // 记录当前节点值
+          out[out.length-1].push(node.val)
+          // 将出队节点不为空的左右节点再次入队
+          if(node.left) queue.push(node.left)
+          if(node.right) queue.push(node.right)
+        }
+    }
+    // 返回翻转后的结果
+    return out.reverse()
+}
