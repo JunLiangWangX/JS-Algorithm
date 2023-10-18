0a499930c46bc16542f0546e8210aa69e6e7d162
汪军梁 <1428637884@qq.com>
2023-10-18 09:26:19 +0800

    feat:add doublePoint  method

--- 141.LinkedListCycle.js
+++ 141.LinkedListCycle.js
@@ -3,7 +3,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-10-18 09:19:17
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-10-18 09:24:29
+ * @LastEditTime: 2023-10-18 09:26:16
  */
 
 
@@ -29,3 +29,26 @@ function hashMap(head){
     return false;
 }
 
+
+/**
+ * @description: 双指针  TC:O(n)  SC:O(1)
+ * @author: JunLiangWang
+ * @param {*} head 给定链表头节点
+ * @return {*}
+ */
+function doublePoint(head){
+    /**
+     * 本方案采用双指针的方式，定义两指针slow，fast。
+     * slow每次移动一个节点，fast每次移动两个节点，如
+     * 果链表存在环，则在某时刻slow与fast指针将重合，
+     * 如何不存在环，则能正常遍历完成链表
+     */
+    if(!head)return false
+    let slow=head,fast=head.next
+    while(fast&&fast.next){
+        if(slow==fast)return true
+        slow=slow.next;
+        fast=fast.next.next;
+    }
+    return false
+}
