98103b3745128647e588f9af74edce54154a317f
汪军梁 <1428637884@qq.com>
2023-10-24 09:35:49 +0800

    feat:add iteration method

--- 145.BinaryTreePostorderTraversal.js
+++ 145.BinaryTreePostorderTraversal.js
@@ -3,7 +3,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-10-24 09:29:41
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-10-24 09:32:29
+ * @LastEditTime: 2023-10-24 09:34:28
  */
 
 
@@ -40,4 +40,32 @@ function dfs(root){
     recursion(root)
     // 返回结果
     return outArray
+}
+
+
+/**
+ * @description: 迭代法  TC:O(n)  SC:O(n)
+ * @author: JunLiangWang
+ * @param {*} root 给定树的头节点
+ * @return {*}
+ */
+function iteration(root){
+    /***
+     * 本方案使用迭代法，利用迭代法对树进行
+     * 前序遍历相信大家都比较清楚，而后序遍
+     * 历与前序遍历相同，只不过返回结果时将
+     * 前序遍历结果翻转就是后序遍历了
+     */
+
+    // 以下为利用迭代对树进行前序遍历
+    if(!root)return [];
+    const stack=[root],outArray=[]
+    while(stack.length>0){
+        let peek=stack.pop()
+        outArray.push(peek.val)
+        if(peek.left)stack.push(peek.left)
+        if(peek.right)stack.push(peek.right)
+    }
+    // 将结果翻转则是后序遍历
+    return outArray.reverse();
 }
