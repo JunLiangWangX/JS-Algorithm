9e6eb68ec268ac86b914673baed6513457daf63a
汪军梁 <1428637884@qq.com>
2023-11-22 09:20:05 +0800

    feat:add hashMap method

--- 242.ValidAnagram.js
+++ 242.ValidAnagram.js
@@ -3,7 +3,7 @@
  * @Author: JunLiangWang
  * @Date: 2023-11-22 09:08:29
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2023-11-22 09:12:25
+ * @LastEditTime: 2023-11-22 09:17:48
  */
 
 
@@ -14,7 +14,7 @@
  * @param {*} t 给定字符串t
  * @return {*}
  */
-function array(s,t){
+function array(s, t) {
     /**
      * 本题使用数组的方式，由于字符串中仅
      * 存在小写字母，因此我们可以通过构建
@@ -22,17 +22,57 @@ function array(s,t){
      * 中a-z的字符数量即可
      */
     // 如果两者长度不一致，肯定不为字母异位词
-    if(s.length!=t.length)return false
+    if (s.length != t.length) return false
     // 定义一个26位的数组记录单词数量
-    let wordList=new Array(26).fill(0)
+    let wordList = new Array(26).fill(0)
     // 遍历字符串
-    for(let i=0;i<s.length;i++){
+    for (let i = 0; i < s.length; i++) {
         // 记录单词数量
-        wordList[s.charCodeAt(i)-97]++;
-        wordList[t.charCodeAt(i)-97]--;
+        wordList[s.charCodeAt(i) - 97]++;
+        wordList[t.charCodeAt(i) - 97]--;
     }
     // 遍历数组，如果某个单词数量不为0，则
     // 证明两字符串中该单词数量不一样
-    for(let i=0;i<26;i++)if(wordList[i]!=0)return false
+    for (let i = 0; i < 26; i++)if (wordList[i] != 0) return false
+    return true
+}
+
+
+
+/**
+ * @description: 哈希表  TC:O(n)  SC:O(n)
+ * @author: JunLiangWang
+ * @param {*} s 给定字符串s
+ * @param {*} t 给定字符串t
+ * @return {*}
+ */
+function hashMap(s, t) {
+    /**
+     * 本题使用哈希表的方式，与上述使用
+     * 数组的方式思路一样，利用哈希表记
+     * 录字符串中单词的数量
+     */
+
+    // 如果两者长度不一致，肯定不为字母异位词
+    if (s.length != t.length) return false
+    // 定义哈希表
+    let map = new Map
+    // 遍历字符串s，在hashMap中记录单词
+    for (let i = 0; i < s.length; i++) {
+        let count = map.get(s[i])
+        map.set(s[i], count == undefined ? 1 : count + 1)
+    }
+    // 遍历字符串t
+    for (let i = 0; i < t.length; i++) {
+        let count = map.get(t[i])
+        // 如果hashMap中单词数量为0，或没有
+        // 这个单词，证明两字符单词数量不一致
+        if (count == 0 || count == undefined) return false
+        map.set(t[i], count - 1)
+    }
+    // 遍历hashMap，如果存在数量不为0的单词
+    // 证明两字符单词数量不一致
+    for (let key in map) if (map.get(key) != 0) return false
+
     return true
 }
