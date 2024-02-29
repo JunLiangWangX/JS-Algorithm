70427ee015f55c4309550eb4552dfc05c93f92f9
汪军梁 <1428637884@qq.com>
2024-02-29 10:36:51 +0800

    feat:add dpOptimization method

--- 198.HouseRobber.js
+++ 198.HouseRobber.js
@@ -4,7 +4,7 @@
  * @Author: JunLiangWang
  * @Date: 2024-02-29 10:18:25
  * @LastEditors: JunLiangWang
- * @LastEditTime: 2024-02-29 10:29:10
+ * @LastEditTime: 2024-02-29 10:36:20
  */
 
 
@@ -36,4 +36,33 @@ function dp(nums){
     }
     // 返回值
     return Math.max(nums[nums.length-1],nums[nums.length-2])
+}
+
+/**
+ * @description: 动态规划优化  TC:O(n)  SC:O(1)
+ * @param {*} nums  给定非负数组
+ */
+function dpOptimization(nums){
+    /**
+     * 上述DP使用了数组存储结果，并且每次都会重复遍历
+     * 获取之前元素的最大组合数，为此我们可以使用滚动
+     * 数组进行优化，在每个时刻只需要存储前两个元素的
+     * 最大组合数即可。
+     */
+
+    // 如何数组长度为1，直接返回唯一元素即可
+    if(nums.length==1)return nums[0]
+    // 获取前两个元素的最大组合数
+    let first=nums[0],second=Math.max(nums[0], nums[1]);
+    // 遍历数组
+    for(let i=2;i<nums.length;i++){
+
+        let temp=second
+        // 与前二最大组合数相加计算当前元素最大组合数，与前一元素比较
+        // 以获最大组合数
+        second=Math.max(first+nums[i],second)
+        first=temp
+    }
+    // 返回结果
+    return second
 }
